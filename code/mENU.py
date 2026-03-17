ffdad5fa19a238658b48b638adfc6c5dbf006e19
luciana silva <luciana.candido120600@gmail.com>
2026-03-17 08:45:00 -0300

    Adding classes generated from StarUml

---
+++ code/mENU.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class MENU:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
+
+    def Operation1(self, ):
+        pass
